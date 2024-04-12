df6054c3ff653a5fa15c16d7c1fa79b8bb38c8ec
prudhvi <prudhvi.ece14@outlook.com>
2024-04-12 12:53:17 +0530

    adding new changes to the framework....

--- projectA/src/main/java/org/projectA/testCases/validlogin.java
+++ projectA/src/main/java/org/projectA/testCases/validlogin.java
@@ -1,16 +1,51 @@
 package org.projectA.testCases;
 
+import org.apache.commons.io.FileUtils;
+import org.json.JSONException;
 import org.projectA.pageObjects.LoginPage;
 import org.projectA.utilities.Base;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompare;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 public class validlogin extends Base {
 
     @Test
-    public void logInTest() {
+    public void JSONCOMPARETEST() throws IOException, JSONException {
 
-        webDriver.get("https://www.saucedemo.com/");
+
+        String actualJSon = FileUtils.readFileToString(new File("C:\\Users\\prudh\\IdeaProjects\\ProjectA\\projectA\\src\\main\\java\\org\\projectA\\testData\\payload\\ActualJson.json"));
+        String expectedJSon = FileUtils.readFileToString(new File("C:\\Users\\prudh\\IdeaProjects\\ProjectA\\projectA\\src\\main\\java\\org\\projectA\\testData\\payload\\ExpectedJson.json"));
+
+        System.out.println(actualJSon);
+        System.out.println("==============");
+        System.out.println(expectedJSon);
+        System.out.println("==============");
+
+        JSONAssert.assertEquals(expectedJSon, actualJSon, JSONCompareMode.STRICT);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*webDriver.get("https://www.saucedemo.com/");
         LoginPage loginPage = new LoginPage(webDriver);
 
         Assert.assertTrue(loginPage.$userName.isDisplayed(), "UserName Field is not DIsplayed");
@@ -20,6 +55,4 @@ public class validlogin extends Base {
         loginPage.typePassword("secret_sauce");
         loginPage.clickOnLogin();
 
-        Assert.assertEquals(webDriver.getTitle(), "Swag Labs");
-    }
-}
+        Assert.assertEquals(webDriver.getTitle(), "Swag Labs");*/
--- projectA/src/main/java/org/projectA/utilities/JsonUtils.java
+++ projectA/src/main/java/org/projectA/utilities/JsonUtils.java
@@ -1,41 +1,34 @@
 package org.projectA.utilities;
 
 import lombok.NoArgsConstructor;
+import org.apache.commons.io.FileUtils;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
 @NoArgsConstructor
-public class JsonUtils {
-    public void updateJsonPayload(String payloadPath, Map<String, String> data) {
+public final class JsonUtils {
+    public static void updateJsonPayload(String payloadPath, Map<String, String> data) {
         try {
             Set<String> keys = data.keySet();
 
             File file = new File(payloadPath);
 
-            String stringJson = "";
-            Scanner sc = new Scanner(file);
-            while (sc.hasNextLine()) stringJson += sc.nextLine();
+            String stringJson = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
 
-            String result = stringJson;
             for (String key : keys)
-                result = result.replace("$" + key, data.get(key));
+                stringJson = stringJson.replace("@" + key, data.get(key));
 
-            //Update your values in the JSON object...
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(payloadPath));
-            bufferedWriter.write(result);
-            bufferedWriter.close();
+            //Update your values in the JSON object...+
+            FileUtils.write(file, stringJson, StandardCharsets.UTF_8, false);
         } catch (IOException ioException) {
             throw new RuntimeException(ioException);
         }
-    }
-
-    public static void main(String[] args) {
-        new JsonUtils().updateJsonPayload("C:\\Users\\Prudhvi\\IdeaProjects\\projectA\\src\\main\\java\\org\\projectA\\testData\\payload\\LoginPayload_POST.json", Map.of("name", "prudhvi", "state", "Andhra Pradesh"));
     }
 }
